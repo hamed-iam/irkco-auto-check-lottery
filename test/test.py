93640ae71db7b2753802e5950355aa2ebca869ec
Hamed Ghazali <72544924+hamed-iam@users.noreply.github.com>
2020-11-14 17:50:43 +0330

    Update test.py

--- test/test.py
+++ test/test.py
@@ -11,13 +11,14 @@ loginUser.click()
 time.sleep(2)
 loginCode = driver.find_element_by_xpath('//*[@id="mainIcons"]/div/div[2]/div/div/section/div/div[5]/div[2]/form/fieldset[2]/div[1]/input');
 passcode = driver.find_element_by_xpath('//*[@id="passwordNew"]')
-loginCode.send_keys('username');
-passcode.send_keys('password')
+loginCode.send_keys(username);
+passcode.send_keys(password)
 time.sleep(2)
 loginbtn = driver.find_element_by_xpath('//*[@id="LoginButton"]')
 loginbtn.click();
 time.sleep(2)
 driver.get('https://esale.ikco.ir/#!/customer/lottery')
 time.sleep(2)
-driver.find_element_by_xpath('//*[@id="mainIcons"]/div/div[2]/div/div/section/div/form').get_text()
-# driver.get_screenshot_as_file('testResult.png')
+driver.find_element_by_xpath('//*[@id="mainIcons"]/div/div[2]/div/div/section/div/form')
+driver.get_screenshot_as_file('testResult.png')
+driver.close()
